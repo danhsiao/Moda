df67f71a03df6300f9d0baf729ea0c42128d1a28
davgreenberg <dgreenberg8@wisc.edu>
2024-10-18 21:14:51 -0500

    Rearranged files and added archive to the app router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import './styles/App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from "./Home"
+import Home from "./pages/Home"
 import About from './pages/About';
 import Join from './pages/Join';
+import Archive from './pages/Archive';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/join' element={<Join/>}/>
+        <Route path='/archive' element={<Archive/>}/>
       </Routes>
     </>
   );
