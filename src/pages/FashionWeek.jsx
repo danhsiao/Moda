a592ada137f12dd09a3c0e963ba535aa8a36aabf
Holly Croke <holl0105c@gmail.com>
2024-10-24 16:50:48 -0500

    Add logos and video

--- src/pages/FashionWeek.jsx
+++ src/pages/FashionWeek.jsx
@@ -5,46 +5,60 @@ function FashionWeek() {
         <div>
             <div>
                 <h1>UW Fashion Week</h1>
-                <h1>Spring '24</h1>
-                <img src="FashionWeekSpring24.jpg"></img>
+                <h2>Spring '24</h2>
+                <img class="main-image" src="FashionWeekSpring24.jpg"></img>
+                <h2 class="description">Moda’s execution of UW Fashion Week in the Spring of 2024 was a spectacle of artistic collaboration within the Madison community.
+                </h2>
             </div>
             <div>
                 <h1>Fuse: Igniting Innovation</h1>
-                <h1>Fashion Show at Orpheum</h1>
-                <h1>Spring '24</h1>
-                <h1>April 19th</h1>
-                <img src="FashionWeekSpring24.jpg"></img>
-                <p class="fashion-show-description">Description</p>
+                <h2>Spring '24</h2>
+                <video controls> 
+                    <source src="Fuse Promo.MP4" type="video/mp4"/>
+                </video>
+                <h2 class="description">This fashion show at The Orpheum Theater featured 54 looks from 10 student designers and retailers with live performances from the band Live From Mars and Optima Dance.</h2>
 
-                <h2>Designers</h2>
                 <div class = "grid-container">
-                    <details>
-                        <summary>Designer Name</summary>
-                        <p>Explore</p>
-                    </details>
-                    <details>
-                        <summary>Designer Name</summary>
-                        <p>Explore</p>
-                    </details>
-                    <details>
-                        <summary>Designer Name</summary>
-                        <p>Explore</p>
-                    </details>
-                </div>
-                <h2>Retailers</h2>
-                <div class = "grid-container">
-                    <details>
-                        <summary>Retailer Name</summary>
-                        <p>Explore</p>
-                    </details>
-                    <details>
-                        <summary>Retailer Name</summary>
-                        <p>Explore</p>
-                    </details>
-                    <details>
-                        <summary>Retailer Name</summary>
-                        <p>Explore</p>
-                    </details>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='abbeyxiong.jpg'></img>
+                        <button class="designer-button">Abbey Xiong</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='gracieharshaw.jpg'></img>
+                        <button class="designer-button">Gracie Harshaw</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='upshift.jpg'></img>
+                        <button class="designer-button">Upshift Swap Shop</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='emmafogarty.jpg'></img>
+                        <button class="designer-button">Emma Fogarty</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='rethreads.jpg'></img>
+                        <button class="designer-button">ReThreads</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='edenmeidl.jpg'></img>
+                        <button class="designer-button">Eden Meidl</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='newday.jpg'></img>
+                        <button class="designer-button">A New Day Boutique</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='vaillencorp.jpg'></img>
+                        <button class="designer-button">Ian Valliencourt</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image yasminhill" src='yasminhill.jpg'></img>
+                        <button class="designer-button" >Yasmin Hill</button>
+                    </div>
+                    <div class= "grid-item">
+                        <img class="logo-image" src='qsm.jpg'></img>
+                        <button class="designer-button">Mia Quitlong Staszcuk</button>
+                    </div>
                 </div>
             </div>
         </div>
