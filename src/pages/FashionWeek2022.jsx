b5db2a62f3a985bfa600e7311851b9f0365b3ec0
davgreenberg <dgreenberg8@wisc.edu>
2024-10-25 11:28:33 -0500

    added react-icons dependency and completed carousel component

--- src/pages/FashionWeek2022.jsx
+++ src/pages/FashionWeek2022.jsx
@@ -1,6 +1,6 @@
 import "../styles/FashionWeek.css"
 import { ImageCarousel } from "../components/ImageCarousel";
-import { slides } from "../data/fashionWeek2022Data.json";
+import data from "../data/fashionWeek2022Data.json";
 
 function FashionWeek2022() {
     return (
@@ -10,7 +10,7 @@ function FashionWeek2022() {
                 <h2>Spring '22</h2>
                 <img class='main-image' src=""></img>
             </div>
-            <ImageCarousel data={slides}/>
+            <ImageCarousel data={data.slides}/>
         </div>
     )
 }
