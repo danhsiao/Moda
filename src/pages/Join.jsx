4e140e765455cf05e4304cdecd32f869b72c3471
nejacob <146866557+nejacob@users.noreply.github.com>
2024-10-31 14:04:37 -0500

    Update Join.jsx

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -10,22 +10,24 @@ function Join() {
                     <strong>
                         We are always looking for new writers, photographers and artists to join our team!
                         Anyone is welcome to join as a contributing member and does not need to apply.
-                        Feel free to send any questions to <a href="modamagazineuw@gmail.com" style="color: black;">modamagazineuw@gmail.com</a>
+                        Feel free to send any questions to <a style={{color: "black"}} href="mailto:modamagaizneuw@gmail.com">modamagaizneuw@gmail.com.</a>
                     </strong>
                 </p>
             </div>
-            <div>className = "join-block"
+            <div className="separator"></div>
+            <div className = "join-block">
                 <h2><strong>Ongoing Virtual Model Call</strong></h2>
                 <p>
                     <strong>
-                        We are continually accepting modeling applications for during the school year! No experience is needed, all are welcome. Please fill out <a href="https://docs.google.com/forms/d/e/1FAIpQLSeZdwS1YSNmZ7AH6pVFyubPHNIHpy0CphRlyru4jaYXZhj52Q/viewform" style="color: black;">this</a> 
+                        We are continually accepting modeling applications for during the school year! No experience is needed, all are welcome. Please fill out <a style={{color: "black"}} href="https://docs.google.com/forms/d/e/1FAIpQLSeZdwS1YSNmZ7AH6pVFyubPHNIHpy0CphRlyru4jaYXZhj52Q/viewform">this</a> 
                         form if you are interested in getting involved. We would love to have you!
                     </strong>
                 </p>
             </div>
+            <div className="separator"></div>   
         </div>
                         
-    )
+    );
 }
 
 export default Join;
