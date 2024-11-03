10a3f31a6e6fa8dfdcd7fac162094748ac479dc0
Holly Croke <holl0105c@gmail.com>
2024-11-03 11:25:35 -0600

    Added FashionWeek2021

---
+++ src/pages/FashionWeek2021.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/FashionWeek.css';
+import Navbar from "../components/Navbar";
+
+function FashionWeek2021() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>UW Fashion Week April 12-16, 2021</h1>
+
+        </div>
+    )
+}
