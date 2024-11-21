cb41a1bcf519befb0239af73ddf6d0fd0c322f18
nejacob <146866557+nejacob@users.noreply.github.com>
2024-11-21 14:26:56 -0600

    Added fashion section page

---
+++ src/pages/Fashion.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../styles/Sections.css';
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+
+function Fashion() {
+    const fashionSection = require('/Users/nayanajacob/Desktop/Moda Project/Moda/src/data/fashionsection.json');
+    return(
+        <div>
+            <Navbar />
+            <h1>Fashion</h1>
+            <div className="gallery">
+                {
+                    fashionSection.fashioncomponents.map((component,index) => (
+                        <Card key={index} image = {component.image} title = {component.title} description = {component.description} date = {component.date}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Fashion;
