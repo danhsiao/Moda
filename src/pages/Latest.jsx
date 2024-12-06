0d8c24717ab063c0a97b043c35146ae36b8f0066
Holly Croke <holl0105c@gmail.com>
2024-12-05 18:06:38 -0600

    added latest compenents to latest page

--- src/pages/Latest.jsx
+++ src/pages/Latest.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import '../styles/Sections.css';
 import Navbar from "../components/Navbar";
 import Card from "../components/Card";
+import lifestyleData from "../data/lifestyledata.json";
+import cultureData from "../data/culturedata.json";
+import fashionData from "../data/fashionsection.json";
+import artsData from "../data/artData.json";
 
 function Latest() {
     const latestData = require('../data/latestData.json');
+    const sections = {
+        ...lifestyleData,
+        ...cultureData,
+        ...fashionData,
+        ...artsData
+    };
+    const combinedComponents = Object.keys(sections)
+    .flatMap(key => Object.values(sections[key]))
+    .sort((a, b) => new Date(b.date) - new Date(a.date));
     return(
         <div>
             <Navbar />
             <div className="gallery">
                 {
-                    latestData.latestcomponents.map((component,index) => (
-                        <Card key={index} image = {component.image} date = {component.date} title = {component.title} description = {component.description} />
-                    ))
+                    combinedComponents.slice(0, 10).map((component, index) => {
+                        return <Card key={index} image={component.image} title={component.title} description={component.description}
+                            date={component.date}/>
+                    })
                 }
             </div>
         </div>
